df73db7fbe1f8c1311a80c8232aa8fc4b2b0ad84
Alec Sears <76724804+searsam1@users.noreply.github.com>
2022-12-19 04:05:51 -0700

    Create factorial.py
    Return factorial of a number.

---
+++ factorial.py
@@ -0,0 +1,19 @@
+# By AS
+def factorial(n):
+  """
+  Return n! (n factorial): 
+  example: n = 3
+    n! = 1 * 2 * 3
+    n! = 6 
+  """
+  prod = 0 
+  for i in range(2, n+1):
+    prod *= i
+  return prod 
+
+def main():
+  assert factorial(3) == 6
+  print("Tests Passed")
+
+if __name__ == "__main__":
+  main()
